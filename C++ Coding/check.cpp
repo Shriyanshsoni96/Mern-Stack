46d20b3152bb4568bb84a5bfd53e642ed61dbb42
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-02-16 23:51:01 +0530

    bascis

--- C++ Coding/check.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-int nums[]={1,2,3};
-
-cout<<nums.size();
-return 0;
-}
--- C++ Coding/functions/roots.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-int main (){
-    cout<<sqrt(10);
-     cout<<sqrt(10);
-    cout<<cbrt(10);
-}
