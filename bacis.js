a85d86c08a08252fdfebe9604f489b14aea80f49
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-03-29 23:17:56 +0530

    functions in js

--- bacis.js
+++ bacis.js
@@ -1,10 +1,38 @@
-console.log("Hello World"); // Output: Hello World
-function print()
-{
-    for(let i = 9 ; i >= 0 ; i--)
-    {
-        console.log(i);
-    }
-}
+// functions without parameters and without return type
+// function print()
+// {
+//     for(let i = 9 ; i >= 0 ; i--)
+//     {
+//         console.log(i);
+//     }
+// }
 
-print();
+// print();
+
+// functions with parameters and without return type
+// function print(num)
+// {
+//     for(let i = num ; i >= 0 ; i--)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// print(5);
+
+// functions with parameters and with return type   
+// function add(num1, num2)
+// {
+//     return num1 + num2;
+// }
+
+// let result = add(5, 10);
+// console.log(result); // Output: 15
+
+// using arrow function
+const add = (num1, num2) => {
+    return num1 + num2;
+}   
+let result = add(5, 10);
+console.log(result); // Output: 15
+
