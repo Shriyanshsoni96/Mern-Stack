730d46495c66a1250ceb25e666cd944452bf78eb
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-01 20:01:09 +0530

    error handling

---
+++ Basics of Javascript/error_handling.js
@@ -0,0 +1,19 @@
+try
+{   console.log("This is try block");
+    let result = x + result;
+    console.log(result);
+    
+}
+
+catch(error)
+{
+    console.log("This is catch block and error is handled the error is " + error);
+}
+
+finally
+{
+    //finally is function which is used to execute the code which is written in the finally block regardless of whether an error is thrown or not.
+    // ye hmesha chalta he 
+    console.log("This is finally block ye sabse last me chalta he regardless of whether an error is thrown or not hmesha chalta he");
+}
+
