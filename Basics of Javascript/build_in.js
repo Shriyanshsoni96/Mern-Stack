534e15ad39a00a03c7de8266de5aae8c356e78d3
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-01 20:00:48 +0530

    Build in Function Completed

---
+++ Basics of Javascript/build_in.js
@@ -0,0 +1,25 @@
+// this arre build in function of javascript
+// math is the functions which are used to do the mathematical operations in javascript
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.sqrt(16));
+// console.log(Math.pow(2, 3));
+// console.log(Math.abs(-5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+// console.log(Math.round(4.5));
+// console.log(Math.random())
+// console.log(Math.max(1, 2, 3, 4, 5));
+// console.log(Math.min(1, 2, 3, 4, 5));
+
+
+let currentdate = new Date();
+console.log(currentdate);
+console.log(currentdate.getFullYear());
+console.log(currentdate.getMonth());
+console.log(currentdate.getDate());
+console.log(currentdate.getHours());
+console.log(currentdate.getMinutes());
+console.log(currentdate.getSeconds());
+console.log(currentdate.getMilliseconds());
+console.log(currentdate.getTime()); 
