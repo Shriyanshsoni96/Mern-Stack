4da5151546268ee0fd53d9456f654f2e37d977e1
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-03 17:42:26 +0530

    Dom manipulation start

---
+++ DOM MANIPULATION/dom_manipulation.js
@@ -0,0 +1,9 @@
+//  document is used to access the DOM (Document Object Model) and manipulate it
+// DOM is a programming interface for HTML and XML documents.
+//  It represents the page so that programs can change the document structure, style, and content.
+//  The DOM represents the document as a tree of nodes, where each node represents an element, attribute, or piece of text in the document.
+
+// 1. getElementById() method is used to access the element by its id attribute value and it returns the element object.
+// let element = document.getElementById("myElement");
+// console.log(element);
+
