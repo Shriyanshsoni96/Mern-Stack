d9eeb71cdf9c5a9bb94cab78429cf605e0a2ad84
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-06 11:39:32 +0530

    dom manupulation with updating methods

--- DOM MANIPULATION/dom_manipulation.js
+++ DOM MANIPULATION/dom_manipulation.js
@@ -31,4 +31,33 @@
 // let element = document.getElementById("myElement");
 // element.innerHTML = "<p>New content</p>";
 
+//2. outerHTML property is used to get or set the HTML content of an element, including the element itself.
+// When you set outerHTML, the element is replaced by the new HTML content.
+// it can set the whole element in to the new content, it can also be used to remove the element from the DOM by setting it to an empty string.
+// let element = document.getElementById("myElement");
+// element.outerHTML = "<div id='myElement'>New content</div>";
+
+// 3. textContent property is used to get or set the text content of an element.
+// let element = document.getElementById("myElement");
+// element.textContent = "New text content";
+
+// let element = document.getElementById("container");
+// element.textContent;
+
+// 4. innerText property is used to get or set the text content of an element, but it also takes into account the CSS styles and hidden elements.
+// let element = document.getElementById("myElement");
+// element.innerText = "New text content";
+
+// 5. setAttribute() method is used to set the value of an attribute on an element.
+// let element = document.getElementById("myElement");
+// element.setAttribute("class", "newClass");
+
+// 6. removeAttribute() method is used to remove an attribute from an element.
+// let element = document.getElementById("myElement");
+// element.removeAttribute("class");
+
+// Creating and inserting DOM elements
+// 1. createElement() method is used to create a new element.
+// let newElement = document.createElement("p");
+// newElement.textContent = "This is a new paragraph.";
 
