3472f76dd15ed3c1bdf7c8bfeee1c4171cdb8cbd
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-07 12:23:33 +0530

    Adding child elements using before end , after end , after begin,  before begin

--- DOM MANIPULATION/adding_element.js
+++ DOM MANIPULATION/adding_element.js
@@ -1,8 +1,32 @@
-let fheading = document.createElement('h1');
-fheading.textContent =" this is the new heading added by append child function ";
+// let fheading = document.createElement('h1');
+// fheading.textContent =" this is the new heading added by append child function ";
 
-let bodytag = document.querySelector('body');
-bodytag.appendChild(fheading);
+// let bodytag = document.querySelector('body');
+// bodytag.appendChild(fheading);
+// using appendchild method we can add the element at the end of the parent element.
 
-bodytag;
+// now we will see how to add the element at the just before the method or elememt we .
+// let fheading = document.createElement('h1');
+// fheading.textContent =" this is the new heading by before begin method ";
+
+// let addingtag =document.querySelector("#mydiv");
+// addingtag.insertAdjacentElement("beforebegin",fheading);
+
+// now we will see how to add the element at the just after the method or elememt we .
+// let fheading = document.createElement('h1');
+// fheading.textContent =" this is the new heading by after begin method ";   
+// let addingtag =document.querySelector("#mydiv");
+// addingtag.insertAdjacentElement("afterbegin",fheading);
+
+// now we will see how to add the element at just after the elememt end .
+// let fheading = document.createElement('h1'); 
+// fheading.textContent =" this is the new heading by after end method ";   
+// let addingtag =document.querySelector("#mydiv");
+// addingtag.insertAdjacentElement("afterend",fheading);
+
+// now we will see how to add the element at just before the elememt end.
+// let fheading = document.createElement('h1'); 
+// fheading.textContent =" this is the new heading by before end method ";   
+// let addingtag =document.querySelector("#mydiv");
+// addingtag.insertAdjacentElement("beforeend",fheading);
 
