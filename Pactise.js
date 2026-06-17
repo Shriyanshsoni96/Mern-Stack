302a38ee226a59d7f6e53c178e92e78c786601d2
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-06-17 11:58:35 +0530

    practise question

--- Pactise.js
+++ Pactise.js
@@ -1,8 +1,23 @@
-const names =["gagan","ishan","yogesh"];
+// const names =["gagan","ishan","yogesh"];
 
-// console.log(names.toUpperCase());
+// // console.log(names.toUpperCase());
 
-const arr3 = names.map(function(names){
-   return names.toUpperCase();
-})
-console.log(arr3);
+// const arr3 = names.map(function(names){
+//    return names.toUpperCase();
+// })
+// console.log(arr3);
+
+// practise question
+// const product=[
+//     {id:1, name: "laptop"},
+//     {id:2, name: "Phone"},
+//     {id:1, name: "Tablet"},
+// ];
+
+// const q2ar=product.map(function(product)
+// {
+//     return product.name;
+// })
+// console.log(q2ar);
+// const q1=product.map(product=>product.name);
+//     console.log(q1);
