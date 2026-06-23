612a0d8fe53169d5d0b3915f1017cb6d93e52243
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-06-23 12:15:11 +0530

    task 2

---
+++ OPPs/inhertance.js
@@ -0,0 +1,78 @@
+// Inheritance allows one class to acquire properties and methods of another class.
+
+// class animal{
+//     eat()
+//     {
+//         console.log("eating");
+//     }
+//     sleep()
+//     {
+//         console.log("sleeping");
+//     }
+// }
+
+// class Dog extends animal{
+//     bark()
+//     {
+//         console.log("barking");
+//     }
+// }
+
+// const dog=new Dog();
+// {
+//  dog.eat();
+//  dog.sleep();
+//  dog.bark();
+
+// }
+
+// task 1 
+// class bike{
+//     model()
+//     {
+//         console.log("2022");
+//     }
+//     name()
+//     {
+//         console.log("Shine sp 125");
+//     }
+    
+
+// }
+
+// const b = new bike();
+// {
+//   b.model();
+//   b.name();
+// }
+
+
+// task 2 multiple inheritance
+
+// class car{
+//     engine()
+//     {
+//         console.log(" V12 turbo Desile engine");
+//     }
+//     brand()
+//     {  
+//         console.log(" BMW ");
+//     }
+
+// }
+
+// class m4 extends car{
+//     model(){
+//         console.log(" 2026 premium model");
+//     }
+    
+// }
+// const Car = new m4();
+// {
+//   Car.engine();
+//   Car.brand();
+//   Car.model();
+// }
+
+
+
