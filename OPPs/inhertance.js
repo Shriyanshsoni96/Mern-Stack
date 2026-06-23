3c99015f302b7b7ef91f41ff08ab79a57e58ca0e
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-06-23 14:44:15 +0530

    task complete

--- OPPs/inhertance.js
+++ OPPs/inhertance.js
@@ -75,4 +75,132 @@
 // }
 
 
+// task 3 herarchical model 
 
+// class vehical{
+//     start()
+//      {
+//     console.log("vehical started");
+//      }
+//     types()
+//     {
+//         console.log("Bike ,Car ,Truck ");
+//     }
+// }
+
+// class Car extends vehical{
+//      drive()
+//      {
+//         console.log("drive the car");
+//      }
+// }
+
+
+//  class sportscar extends vehical
+//  {
+//      drive()
+//      {   
+//         console.log("Drive the Sports car ");
+//      }
+//   }
+ 
+// const sport = new sportscar();
+// {
+//     sport.start();
+//     sport.drive();
+// } 
+// const car = new Car();
+// {
+//     car.start();
+//     car.drive();
+// }
+
+
+//task 4 
+
+// class vehical{
+//     start()
+//      {
+//     console.log("vehical started");
+//      }
+//     types()
+//     {
+//         console.log("Bike ,Car ,Truck ");
+//     }
+// }
+
+// class Car extends vehical{
+//      drive()
+//      {
+//         console.log("drive the car");
+//      }
+// }
+
+
+//  class sportscar extends Car
+//  {
+//      tubomode()
+//      {   
+//         console.log("Turbo mode on  ");
+//      }
+//   }
+ 
+// const sport = new sportscar();
+// {
+//     sport.start();
+//     sport.drive();
+//     sport.tubomode();
+// } 
+
+
+//task 5 
+
+ class vehical{
+    start()
+     {
+    console.log("vehical started");
+     }
+    types()
+    {
+        console.log("Bike ,Car ,Truck ");
+    }
+}
+
+class bike extends vehical{
+    model(){
+        console.log("latest model of ATHER");
+    }
+    mft(){
+        console.log("2026");
+    }
+}
+
+class Car extends vehical{
+     drive()
+     {
+        console.log("drive the car");
+     }
+}
+
+
+ class sportscar extends Car
+ {
+     tubomode()
+     {   
+        console.log("Turbo mode on  ");
+     }
+  }
+
+  const B =new bike();
+  {
+    B.start();
+    B.mft();
+    B.model();
+  }
+ 
+const sport = new sportscar();
+{
+    sport.start();
+    sport.drive();
+    sport.tubomode();
+} 
