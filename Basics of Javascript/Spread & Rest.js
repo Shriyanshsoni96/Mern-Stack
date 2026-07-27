4c5fb898082e7a01347ff2a6275c51afc11f25e5
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-07-27 22:28:44 +0530

    spread & Rest

---
+++ Basics of Javascript/Spread & Rest.js
@@ -0,0 +1,17 @@
+// Used to concatinate of array or joining of array
+
+// const frontend=[" HTML "," CSS " ];
+// const backend=[" NODE.JS "," EXPRESS.JS "," MONGODB "];
+
+// const Full_stack=[...frontend,...backend];// used for concatination 
+// console.log(Full_stack)
+
+
+// ... can mean Spread or Rest depending on where it's used.
+// Spread expands arrays or objects and is commonly used for copying, merging, and passing function arguments.
+// Rest collects multiple values into an array or object and is commonly used in function parameters and destructuring.
+// Spread creates shallow copies, not deep copies.
+
+
+
+
