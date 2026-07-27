d0f0c97abe78f9e918427fffff6a52f4a149452b
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-07-27 22:15:39 +0530

    destructing of array

--- Basics of Javascript/Array Destructuring.js
+++ Basics of Javascript/Array Destructuring.js
@@ -1,7 +1,32 @@
-const alphabet = ["a","b","c","d"];
+// Extract the values from an array and store them in variable is known as array destructing 
 
-const[first, second, third]=alphabet;
 
-console.log(first);
-console.log(second);
-console.log(third);
+// const alphabet = ["a","b","c","d"];
+// const[first, second, third]=alphabet;
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// const colors=["Red","Green","Blue"]
+// const [clr_1,clr_2,clr_3]=colors;
+// console.log(clr_1)
+// console.log(clr_2)
+// console.log(clr_3)
+
+
+
+//using of rest 
+// const colors=["Red","Green","Blue","Orange"]
+// const [clr_1,clr_2,...rest] = colors;
+// console.log(clr_1)
+// console.log(clr_2)
+// console.log(rest)
+
+// Skipping values 
+const colors=["Red","Green","Blue","Orange"]
+const [clr_1,clr_2,,clr_4] = colors;
+console.log(clr_1)
+console.log(clr_2)
+console.log(clr_4)
+
+
