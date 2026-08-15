3328d0290c5ae3c9247119c0cb28377aa37baf31
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-08-15 18:24:08 +0530

    os module

---
+++ NODE PRACTISE/module basics/OS_module.js
@@ -0,0 +1,46 @@
+const os = require("os");
+
+//? Platform method i used to show the current platform which we are using  
+console.log("Platform :-",os.platform());
+
+//? Method is used to return the user information in object format
+// console.log("User  :-",os.userInfo());
+
+//? Method is used to return the user name 
+// console.log("User  :-",os.userInfo().username);
+
+//? Method is used to return the cpu architecture
+// console.log("Architecture :-",os.arch());
+
+//? Method is used to return the free memory in the cpu in bytes
+// console.log("Free Memory :-",os.freemem() ,"bytes");
+
+//? Method is used to return the total memory in the cpu in bytes
+// console.log("Total Memory :-",os.totalmem() ,"bytes");
+
+//? Method is used to return the cpu uptime in seconds
+// console.log("CPU uptime :-",os.uptime() ,"seconds");
+
+//? Method is used to return the home directory 
+// console.log("Home directory :-",os.homedir());
+
+//? Method is used to return the hostname
+// console.log("Host name :-",os.hostname());
+
+//? Method is used to return the object with deatils about the network interface
+// console.log("Network interface :-",os.networkInterfaces());
+
+//? Method is used to return the cpu logical core in object
+// console.log("Cpu core:-",os.cpus());
+
+//? Method is used to return the temprery directory
+// console.log("Temprary Directory  :-",os.tmpdir());
+
+//?  Method is used to return the opereating system type which is currenlty used 
+// console.log("Operating system :-",os.type());
+
+
+
+
+
+
