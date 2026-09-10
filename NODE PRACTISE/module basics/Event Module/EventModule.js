64f940d06487d55722c2fbe4ff2bce45d2423a4a
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-09-10 20:41:08 +0530

    Event modue learning

--- NODE PRACTISE/module basics/Event Module/EventModule.js
+++ NODE PRACTISE/module basics/Event Module/EventModule.js
@@ -1,1 +1,26 @@
+ const EventEmitter= require("events"); 
+ //? we are importing the Event module in this line
+
+ const emitter = new EventEmitter();
+ //? we create the instance in of event module
+ //? Event Emitter node.js ka ek special object/class hai jo event trigger , listen , or register krta he 
+ 
+ //! .on() method is used to create or listen the event 
+ //! .on() kisi event ke liye listener register karta hai.
+//  emitter.on("greet",(user)=>{
+//     console.log("Hello "+user);
+//  });
+  
+// //! .emit() kisi event ko trigger/fire karta hai.
+//  emitter.emit("greet" ,"Radhe Shayam");
+ 
+ //? if we want to pass the no of arguments in single times so we create the object 
+ 
+ emitter.on("greet" , (arg) =>{
+
+    console.log(`Hello ${arg.name}, Proffesion ${arg.prof} , AGE ${arg.age}`);
+ 
+});
+
+ emitter.emit("greet" ,{ name:"Radhe Shayam" ,prof:"Full Stack Developer", age:"24"});
  
