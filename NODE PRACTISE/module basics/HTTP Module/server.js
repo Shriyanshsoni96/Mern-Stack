686c6e6ddfe831d4bec7c36bbe704c4cf36314a9
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-09-23 23:10:14 +0530

    ch

--- NODE PRACTISE/module basics/HTTP Module/server.js
+++ NODE PRACTISE/module basics/HTTP Module/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
     if(req.url === "/")
     {   res.setHeader("Content-Type", "text/plain");
-        res.write("this is the server that you created last night ");
+        res.write("this is the server that you created last night so now i want something from you if you dont give it to me then i will fight for that and gain from the team , so whats the new thing in this ");
         res.end();
     }
     if(req.url === "/source")
